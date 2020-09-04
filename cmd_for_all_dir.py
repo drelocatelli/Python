62a25e9a05b6026f7bf73d8e89e48f8bda1500fa
andressa <you@example.com>
2020-09-04 18:07:40 -0300

    ..

--- cmd_for_all_dir.py
+++ cmd_for_all_dir.py
@@ -1,9 +1,17 @@
 import os
 
-opt = input("[COMANDO PRA TODOS DIRETÓRIOS] $ ")
+input("POSICIONE ESTE SCRIPT UM NÍVEL DE DIRETÓRIO ANTERIOR [ENTER] $ ")
+
+opt = False
+while(opt == "" or bool(opt) == False):
+    opt = input("[COMANDO PRA TODOS DIRETÓRIOS] $ ")
+    continue
 
 for x in os.listdir('.'):
     if os.path.isdir(x):
-        cmd = f"cd {x} && git pull"
+        cmd = f"cd {x} && {opt}"
+        print("------------------------------------------------")
         print("$ "+cmd)
+        print("------------------------------------------------")
         os.system(f"cd {x} && {opt}")
+        
