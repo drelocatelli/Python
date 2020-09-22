abebd8caf5455cd375b40ff80c7cb43e4368c1e9
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-09-22 10:22:25 -0300

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,10 +3,3 @@ import os
 def sendMsg(token, groupId, msg):
     print('curl -d chat_id=-'+groupId+' -d text="'+msg+'"  https://api.telegram.org/'+token+'/sendMessage')
     os.system('curl -d chat_id=-'+groupId+' -d text="'+msg+'"  https://api.telegram.org/bot'+token+'/sendMessage')
-    
-
-token = input("[BOT TOKEN] $ ")
-groupId = input("[GROUP ID NUMBER] $ ")
-msg = input("[MSG] $ ")
-
-sendMsg(token=token, groupId=groupId, msg=msg)
