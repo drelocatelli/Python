ae5f44097c649c857a81732d41e9988bced2a923
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-09-25 21:46:30 -0300

    Update install_Pyqt5.py

--- install_Pyqt5.py
+++ install_Pyqt5.py
@@ -1,8 +1,12 @@
 import os
+opt = input("Instalar pyqt5? [y] $ ").lower()
 
-os.system("pip install PyQt5")
-os.system("sudo apt-get install python3-pyqt5")
-os.system("sudo apt-get install qttools5-dev-tools")
-os.system("echo 'alias pyqt=/usr/lib/x86_64-linux-gnu/qt5/bin/designer' >> ~/.bashrc && source ~/.bashrc")
+if opt == "y":
+    os.system("pip install PyQt5")
+    os.system("sudo apt-get install python3-pyqt5")
+    os.system("sudo apt-get install qttools5-dev-tools")
+    os.system("echo 'alias pyqt=/usr/lib/x86_64-linux-gnu/qt5/bin/designer' >> ~/.bashrc && source ~/.bashrc")
 
-print('\n\n run command: $ pyqt')
+    print('\n\n run command: $ pyqt')
+else:
+    print("convert ui to py cmd: $ pyuic5 -x input.ui -o output.py")
