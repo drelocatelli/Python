2fb22c819962a054c37b8c40ae7b192b487f3ed4
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-09-27 16:35:09 -0300

    Add files via upload

--- sys_args.py
+++ sys_args.py
@@ -1,5 +1,7 @@
 import sys
 
+# type example: python append_list_within_dictionary.py  "{'nome': 'Andressa', 'cpf': 100}"
+
 list_dict = [{'nome': 'Ana', 'cpf': '1000'}, {'nome': 'João', 'cpf': '7770'}]
 app_item = eval(sys.argv[1].replace("\"", ""))
 
