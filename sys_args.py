77fa101ef4ab5475c7035c8e5950dab900fb8819
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-09-27 16:38:44 -0300

    Update sys_args.py

--- sys_args.py
+++ sys_args.py
@@ -3,7 +3,8 @@ import sys
 # type example: python append_list_within_dictionary.py  "{'nome': 'Andressa', 'cpf': 100}"
 
 list_dict = [{'nome': 'Ana', 'cpf': '1000'}, {'nome': 'João', 'cpf': '7770'}]
-app_item = eval(sys.argv[1].replace("\"", ""))
+app_item = sys.argv[1].replace("\"", "")
+app_item = eval(app_item)
 
 list_dict.append(app_item) 
 
