710f2e996fc0cddc483ac53186bd04921c9d256e
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-09-28 09:39:44 -0300

    Update sys_args.py

--- sys_args.py
+++ sys_args.py
@@ -1,11 +1,17 @@
 import sys
 
-# type example: python append_list_within_dictionary.py  "{'nome': 'Andressa', 'cpf': 100}"
+print("\033[38;2;255;255;255m\033[1;41mNão se esqueça de passar os parâmetros\033[0m\n\n")
+# python append_list_within_dictionary.py  "{'nome': 'Ana', 'cpf': '1000'}, {'nome': 'João', 'cpf': '7770'}, {'nome': 'Andressa', 'cpf': 100},{'nome': 'Dory', 'cpf': 100}"
 
-list_dict = [{'nome': 'Ana', 'cpf': '1000'}, {'nome': 'João', 'cpf': '7770'}]
-app_item = sys.argv[1].replace("\"", "")
-app_item = eval(app_item)
+list_dict = []
+app_item = sys.argv[1]
 
-list_dict.append(app_item) 
+list_dict.append(app_item)
+list_dict = str(list_dict)
+list_dict = list_dict.replace("\"","")
+
+# eval sempre por ultimo!!!!!!!!!!!!!!!!
+list_dict = eval(list_dict)
 
 print(list_dict)
+
