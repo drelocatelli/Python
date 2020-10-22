0b4e1f25a20ab4b0ab77fecb63f8d3803569dbc5
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-10-22 11:23:14 -0300

    Update cmd_for_all_dir.py

--- cmd_for_all_dir.py
+++ cmd_for_all_dir.py
@@ -1,17 +1,14 @@
 import os
 
-input("POSICIONE ESTE SCRIPT UM NÍVEL DE DIRETÓRIO ANTERIOR [ENTER] $ ")
+input("\033[38;2;255;255;255m\033[1;41mPOSICIONE ESTE SCRIPT UM NÍVEL DE DIRETÓRIO ANTERIOR [ENTER] $ \033[0m")
 
 opt = False
 while(opt == "" or bool(opt) == False):
-    opt = input("[COMANDO PRA TODOS DIRETÓRIOS] $ ")
+    opt = input("\033[1;32m[COMANDO PRA TODOS DIRETÓRIOS] $ \033[0m")
     continue
 
 for x in os.listdir('.'):
     if os.path.isdir(x):
         cmd = f"cd {x} && {opt}"
-        print("------------------------------------------------")
-        print("$ "+cmd)
-        print("------------------------------------------------")
+        print("\033[1;32m\n$ "+cmd+"\033[0m")
         os.system(f"cd {x} && {opt}")
-        
