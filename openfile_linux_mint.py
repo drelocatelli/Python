781fe63d55bb7669bda3890a1b40ca06a69367b3
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-10-29 09:13:23 -0300

    Update openfile_linux_mint.py

--- openfile_linux_mint.py
+++ openfile_linux_mint.py
@@ -1,4 +1,12 @@
-import os
+import os, sys, optparse
+
+parse = optparse.OptionParser()
+parse.add_option('--way', action='store', dest='way', type='string')
+
+if '--way' in sys.argv:
+    options, remainder = parse.parse_args()
+    os.system(f"xdg-open {options.way}")
+    sys.exit()
 
 print(os.getcwd())
 location = input("Caminho: ")
