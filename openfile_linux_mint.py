25389abb3f51df97b35118098712955b98d49529
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-10-29 09:27:07 -0300

    Update openfile_linux_mint.py

--- openfile_linux_mint.py
+++ openfile_linux_mint.py
@@ -4,13 +4,11 @@ parse = optparse.OptionParser()
 parse.add_option('--way', action='store', dest='location', type='string')
 parse.add_option('--location', action='store', dest='location', type='string')
 parse.add_option('--destination', action='store', dest='location', type='string')
+options, remainder = parse.parse_args()
 
-if bool(parse.parse_args()):
-    options, remainder = parse.parse_args()
+if options.location is not None:
     os.system(f"xdg-open {options.location}")
-    sys.exit()
-
-print("\n"+os.getcwd()+"\n")
-location = input("Caminho: ")
-
-os.system(f"xdg-open {location}")
+else:
+    # print("\n"+(os.getcwd())+"\n")
+    location = input("Caminho: ")
+    os.system(f"xdg-open {location}")
