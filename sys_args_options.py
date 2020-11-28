fe28513785afd2340833f0eee601ca7d05c04a20
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-11-28 11:07:57 -0300

    Update sys_args_options.py

--- sys_args_options.py
+++ sys_args_options.py
@@ -1,8 +1,24 @@
 import sys, optparse
 
 parser = optparse.OptionParser()
-parser.add_option('--title', action="store", dest="title", type="int")
-options, remainder = parser.parse_args()
+parser.add_option('--title', action="store", dest="title", type="string")
+parser.add_option('--teste', action="store", dest="teste", type="string")
+options, args = parser.parse_args()
 
+# printa todos os argumentos
 print(options)
-print(f"title: {options.title}")
+# printa opção
+print(options.title)
+
+
+print('------------------------------------')
+
+# printa quantidade de argumentos
+argumentos = []
+
+for i in sys.argv[1:]:
+    if not i.startswith('--'):
+        argumentos.append(i)
+
+print(argumentos)
+print(len(argumentos))
