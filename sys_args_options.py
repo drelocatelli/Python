8a64a1f209ca8032bd0c2cd919f170b8965fa24a
drelocatelli <32282846+drelocatelli@users.noreply.github.com>
2020-11-28 11:09:58 -0300

    Update sys_args_options.py

--- sys_args_options.py
+++ sys_args_options.py
@@ -1,14 +1,14 @@
 import sys, optparse
 
 parser = optparse.OptionParser()
-parser.add_option('--title', action="store", dest="title", type="string")
-parser.add_option('--teste', action="store", dest="teste", type="string")
+parser.add_option('--opcao1', action="store", dest="opcao1", type="string")
+parser.add_option('--opcao2', action="store", dest="opcao2", type="string")
 options, args = parser.parse_args()
 
 # printa todos os argumentos
 print(options)
 # printa opção
-print(options.title)
+print(options.opcao1)
 
 
 print('------------------------------------')
